f75bdc5ff9a56f2209b97465f31f162807a72090
Derek Anderson <public@kered.org>
2024-09-04 22:36:17 -0700

    a bunch of fixes to search ui

--- src/Search.jsx
+++ src/Search.jsx
@@ -7,15 +7,19 @@ import {
 import {actionClicks, ActionWithText } from './AntdActionUtils'
 import {wait} from './util'
 
+const HTTPS = window.location.protocol=='https:'
+
 
 export function Search(props) {
 
-  const [adding_ip, set_adding_ip] = useState(false)
   const [search_progress, set_search_progress] = useState(null)
   const [local_ip, set_local_ip] = useState('')
   const [ip_and_port, set_ip_and_port] = useState('')
   const [dismissed, set_dismissed] = useState(false)
   const [found_some, set_found_some] = useState(0)
+
+  const search_in_progress = !dismissed && search_progress!=null && search_progress<253
+  const search_done = !search_in_progress && search_progress==253
 
   const found_some_ref = useRef(found_some);
   useEffect(() => {
@@ -23,7 +27,6 @@ export function Search(props) {
   }, [found_some]);
 
   function reset() {
-    set_adding_ip(false)
     set_search_progress(null)
     set_local_ip('')
     set_ip_and_port('')
@@ -49,18 +52,17 @@ export function Search(props) {
   }, [])
 
   const full_ip = ip_and_port.match(ipRegex)
-  const subnet = ip_and_port.match(/([0-9]{1,3}\.){3}[xX]{1,3}/)
+  const subnet = !HTTPS && ip_and_port.match(/([0-9]{1,3}\.){3}[xX]{1,3}/)
 
   let search_button = <StopOutlined />
   if (full_ip) search_button = <LinkOutlined />
   if (subnet) search_button = <SearchOutlined />
-  if (search_progress!=null) search_button = null;
+  if (search_in_progress) search_button = null;
 
   function add_printer(ws, url) {
     console.log('add_printer...', ws, url)
     //wait(2500).then(()=>props.add_printer(ws, url))
     props.add_printer(ws, url)
-    set_adding_ip(false)
     set_found_some(found_some_ref.current+1)
   }
 
@@ -84,7 +86,6 @@ export function Search(props) {
           const timeoutId = setTimeout(() => {
             console.log('close timeout', ws)
             ws.close()
-            reject(new Error('WebSocket connection timed out'));
           }, 10000);
 
           ws.onopen = () => {
@@ -96,7 +97,7 @@ export function Search(props) {
           }
           ws.onclose = () => {
             set_search_progress(search_progress_ref.current+1)
-            resolve()
+            resolve(new Error('WebSocket connection timed out', ws))
           }      
 
         })
@@ -104,9 +105,6 @@ export function Search(props) {
       console.log('discovered_printers', discovered_printers)
     } catch (error) {
       console.log('search done', error)
-    } finally {
-      console.log('search finally')
-      set_search_progress(null)
     }
   }
 
@@ -126,7 +124,7 @@ export function Search(props) {
 
   const actions = [];
 
-  if (search_progress==253) {
+  if (search_done) {
     actions.push(
       <ActionWithText key='search' onActionClick={()=>reset()} icon={<SearchOutlined />}>
         Search Again
@@ -134,7 +132,7 @@ export function Search(props) {
     )
   }
 
-  if (props.printer_count && search_progress==253) {
+  if (props.printer_count && search_done) {
     actions.push(
       <ActionWithText key='dismiss' onActionClick={()=>dismiss()} icon={<CloseCircleOutlined />}>
         Dismiss
@@ -159,34 +157,47 @@ export function Search(props) {
         <Flex vertical align='center' gap='middle'>
           <div style={{fontSize:'16pt'}}><PlusOutlined/> <PrinterTwoTone /></div>
           
-          {search_progress==null ? (
+          {search_in_progress || search_done ? null : (
             <>
               <div>
                 Enter an ip/port (<code style={{fontSize:'smaller'}}>192.168.0.57[:81]</code>)
-                or subnet (<code style={{fontSize:'smaller'}}>192.168.0.x</code>) to search.
+                {HTTPS ? null : <span>or subnet (<code style={{fontSize:'smaller'}}>192.168.0.x</code>) to search</span>}.
               </div>
               <Input.Search 
-                addonBefore="ws://" 
+                addonBefore={HTTPS ? 'wss://' : "ws://"}
                 placeholder="192.168.0.57[:81]" 
                 allowClear 
-                loading={search_progress!=null}
-                disabled={search_progress!=null}
+                loading={search_in_progress}
+                disabled={search_in_progress}
                 enterButton={search_button} 
                 defaultValue={local_ip}
                 onChange={(e)=>console.log(e) || set_ip_and_port(e.target.value)}
                 onSearch={(value)=>search_or_add_printer(value)}
               />
-              <div style={{wordBreak:'break-word'}}>Or try one of these common subnets: <JoinWithComma components={subnet_buttons} /></div>
-            </>
-          ) : (
-            <>
-              {search_progress<253 ? 'Searching '+ip_and_port.toLowerCase()+'...' : null}
-              {search_progress==253 && found_some==0 ? <div>
-                Sorry, no printers found.  Are they on? Did you select the right subnet? <a href='https://nordvpn.com/blog/what-is-a-subnet-mask/#:~:text=you%E2%80%99re%20connected%20to.-,How%20do%20you%20find%20the%20subnet%20mask%3F,-Follow%20the%20guides' target='_blank'><InfoCircleOutlined /></a>
-              </div> : null}
-              <Progress type="circle" percent={Math.round(100*search_progress/254)} />
+              <div style={{wordBreak:'break-word'}}>
+                { HTTPS ? <span>
+                  Subnet search is unavailable over HTTPS. <a href='https://www.cloudflare.com/learning/ssl/what-is-mixed-content/' target='_blank'><InfoCircleOutlined /></a> &nbsp; <Button size='small' onClick={() => window.location = 'http://'+window.location}>Go HTTP</Button>
+                </span> : <span>
+                  Or try one of these common subnets: <JoinWithComma components={subnet_buttons} />
+                </span> }
+              </div>
             </>
           )}
+          { search_in_progress && !search_done ? (
+            <>
+              {search_in_progress ? 'Searching '+ip_and_port.toLowerCase()+'...' : null}
+            </>
+          ) : null}
+          { search_progress ? (
+            <Progress type="circle" percent={Math.round(100*search_progress/253)} />
+          ) : null}
+          { search_done ? (
+            <>
+              {found_some==0 ? <div>
+                Sorry, no printers found.  Are they on? Did you select the right subnet? <a href='https://nordvpn.com/blog/what-is-a-subnet-mask/#:~:text=you%E2%80%99re%20connected%20to.-,How%20do%20you%20find%20the%20subnet%20mask%3F,-Follow%20the%20guides' target='_blank'><InfoCircleOutlined /></a>
+              </div> : null}
+            </>
+          ) : null}
 
         </Flex>
       </Card>
